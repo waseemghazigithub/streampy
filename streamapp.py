28cb2e8216dd55876e1a3364707b2c0f3558506c
waseemghazigithub <159131359+waseemghazigithub@users.noreply.github.com>
2025-02-23 00:31:52 +0500

    default.py

--- streamapp.py
+++ streamapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os 
 from io import BytesIO
-import openpyxl
+# import openpyxl
 
 # Set up the app
 st.set_page_config(page_title="📀 Data Sweeper", layout='wide')
